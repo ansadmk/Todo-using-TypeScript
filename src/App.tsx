9c87e035805b39678ebdcf9660e38a7e94044f69
ansadmk <ansadmk@gmail.com>
2023-09-06 12:04:15 +0530

    delete function

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
-import React, { useState } from 'react'
+import React, { FC, useState } from "react";
 
-
-
-
-interface type{
-  id:Number,
-  content:String,
-  edit:boolean
+interface type {
+  id: Number;
+  content: String;
+  edit: boolean;
 }
 function App() {
-  const [state, setState] = useState<type []>([])
-  
-  const add=(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault()
-    setState((states:any)=>[...states,{id:Math.random(),content:e.target.content.value,edit:false}])      
-  }
+  const [state, setState] = useState<type[]>([]);
+
+  const add= (e:any) => {
+    e.preventDefault();
+    setState((states: any) => [
+      ...states,
+      { id: Math.random(), content: e.target.content.value, edit: false },
+    ]);
+    e.target.reset()
+  };
+  const deletetodo = (i: Number) => {
+    const filtprod: any = state.filter((a: Object, index: Number) => {
+      return index != i;
+    });
+    setState(filtprod);
+  };
   return (
     <>
-    <form action="" onSubmit={add}>
-      <input type="text" id='content' />
-    </form>
-    {state.map((v)=><h1 >{v.content}</h1>)}
+      <form action="" onSubmit={add}>
+        <input type="text" id="content" />
+        <input type="submit" value="add" />
+      </form>
+      {state.map((v, i) => (
+        <h1>
+          {v.content} <button onClick={() => deletetodo(i)}>delete</button>
+        </h1>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
